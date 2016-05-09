16db087f548e6f420bdd3e836618d3b9a5d31104
Rafael Telles <raftel.ti@gmail.com>
2016-05-08 21:05:57 -0300

    Fix _violate_constraints

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ import random
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
 
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
 
 class Cluster:
         def __init__(self, position, _id):
@@ -33,7 +35,7 @@ class ConstrainedKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
         self._debug = debug
         self.n_clusters = n_clusters
 
-    def fit(self, X, y=None, must_link=set(), cannot_link=set()):
+    def fit(self, X, y=None, must_link=[], cannot_link=[]):
         assert type(X).__name__ == 'ndarray', "X must be a ndarray"
         assert X.shape[0] >= self.n_clusters, "X can't have less objects than n_clusters"
 
@@ -48,24 +50,24 @@ class ConstrainedKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
             clusters.append(cluster)
             _id += 1
 
+        return
+
         self.debug("Initial Centroids: ")
         for cluster in clusters:
             self.debug(cluster)
 
-        # Test:
-        must_link = np.asarray([(4, 1)])
-        cannot_link = np.asarray([[6, 3 ]])
-
-        for _ in range(500):
+        for _ in range(100):
             for cluster in clusters:
                 cluster.clear()
             for d in X:
-                ranked_clusters = ConstrainedKMeans._rank_clusters(d, clusters)
+                ranked_clusters = _rank_clusters(d, clusters)
                 for cluster in ranked_clusters:
-                    violate_constraint = ConstrainedKMeans._violate_constraints(d, cluster, must_link, cannot_link)
+                    violate_constraint = _violate_constraints(d, cluster, must_link, cannot_link)
                     if violate_constraint is False:
                         cluster.add(d)
                         break
+                if violate_constraint:
+                    raise IOError("Unable to cluster")
 
             for cluster in clusters:
                 cluster.recalculate()
@@ -78,38 +80,45 @@ class ConstrainedKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
             self.debug(20*"-")
         # ...
 
-    @staticmethod
-    def _rank_clusters(instance, clusters):
-        """
-        Return a ascendant list of nearest clusters of certain instance
-        :param instance: Instance to compare
-        :param clusters: Clusters
-        :return: the clusters array sorted by distance of instance
-        """
-        clusters_ranks = np.asarray([np.linalg.norm(p.position - instance) for p in clusters]).argsort()
-        sorted_clusters = np.asarray(clusters)[clusters_ranks]
+def _rank_clusters(instance, clusters):
+    """
+    Return a ascendant list of nearest clusters of certain instance
+    :param instance: Instance to compare
+    :param clusters: Clusters
+    :return: the clusters array sorted by distance of instance
+    """
+    clusters_ranks = np.asarray([np.linalg.norm(p.position - instance) for p in clusters]).argsort()
+    sorted_clusters = np.asarray(clusters)[clusters_ranks]
 
-        return sorted_clusters
+    return sorted_clusters
 
-    # TODO :soh esta funcionando com numero de variaveis = 1 quand tem links.
-    @staticmethod
-    def _violate_constraints(instance, cluster, must_link, cannot_link):
-        if len(cluster.instances) == 0:
-            return False
-
-        for link in filter(lambda l: instance in l, must_link):
-            other_instance = np.setdiff1d(link, [instance])
-            if other_instance not in cluster.instances:
-                return True
-
-        for link in filter(lambda l: instance in l, cannot_link):
-            other_instance = np.setdiff1d(link, [instance])
-            if other_instance in cluster.instances:
-                return True
-
+def _violate_constraints(instance, cluster, must_link, cannot_link):
+    if len(cluster.instances) == 0:
         return False
 
+    for link in filter(lambda l: _contains(instance, l), must_link):
+        other_instance = link[1] if (link[0] - instance).all() else link[0]
+        if not _contains(other_instance, cluster.instances):
+            return True
+
+    for link in filter(lambda l: _contains(instance, l), cannot_link):
+        other_instance = link[1] if (link[0] - instance).all() else link[0]
+        if _contains(other_instance, cluster.instances):
+            return True
+
+    return False
+
+
+def _contains(instance, _list):
+    """Returns True if the ndarray list contains the specified instance"""
+    return bool([y for y in (instance == _list) if y.all()])
+
+
 if __name__ == '__main__':
-    c = ConstrainedKMeans(n_clusters=3, debug=True)
-    X = np.array([[p] for p in range(10)])
-    c.fit(X)
+
+    digits = load_digits()
+
+    c1 = ConstrainedKMeans(n_clusters=10, debug=False)
+    c1.fit(digits.data)
+    c2 = KMeans(n_clusters=10)
+    c2.fit(digits.data)
