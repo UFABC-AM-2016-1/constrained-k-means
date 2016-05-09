57f49cc604d4c118df07e86649f69a7fc0df0333
Rafael Telles <raftel.ti@gmail.com>
2016-05-09 20:44:40 -0300

    Update

--- __init__.py
+++ __init__.py
@@ -1,27 +1,10 @@
 import numpy as np
-import random
 
-from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
 
+from sklearn.datasets import load_digits, load_iris, load_diabetes
+from sklearn import datasets
 
-class Cluster:
-        def __init__(self, position, _id):
-            self.instances = []
-            self.position = position
-            self._id = _id
-
-        def recalculate(self):
-            self.position = np.mean(self.instances, axis=0)
-
-        def add(self, instance):
-            self.instances.append(instance)
-
-        def clear(self):
-            self.instances = []
-
-        def __str__(self):
-            return str({"id": self._id, "position": self.position})
 
 class ConstrainedKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
 
@@ -33,83 +16,145 @@ class ConstrainedKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
         self._debug = debug
         self.n_clusters = n_clusters
 
-    def fit(self, X, y=None, must_link=set(), cannot_link=set()):
+    def fit(self, X, y=None, must_link=[], cannot_link=[]):
         assert type(X).__name__ == 'ndarray', "X must be a ndarray"
         assert X.shape[0] >= self.n_clusters, "X can't have less objects than n_clusters"
 
         # Initialize cluster centers
         n_samples = X.shape[0]
         samples_idx = np.random.choice(range(n_samples), self.n_clusters, replace=False)
-        centroids = map(lambda i: X[i], samples_idx)
-        clusters = []
-        _id = 0
-        for centroid in centroids:
-            cluster = Cluster(centroid,_id)
-            clusters.append(cluster)
-            _id += 1
+        centroids = np.array(map(lambda i: X[i], samples_idx))
+        clusters = [[] for _ in range(self.n_clusters)]
 
-        self.debug("Initial Centroids: ")
-        for cluster in clusters:
-            self.debug(cluster)
+        labels = [0 for _ in X]
+        inertia = 0
 
-        # Test:
-        must_link = np.asarray([(4, 1)])
-        cannot_link = np.asarray([[6, 3 ]])
+        for _ in range(50):
+            for i in range(self.n_clusters):
+                clusters[i] = []
+                inertia = 0
 
-        for _ in range(500):
-            for cluster in clusters:
-                cluster.clear()
-            for d in X:
-                ranked_clusters = ConstrainedKMeans._rank_clusters(d, clusters)
-                for cluster in ranked_clusters:
-                    violate_constraint = ConstrainedKMeans._violate_constraints(d, cluster, must_link, cannot_link)
-                    if violate_constraint is False:
-                        cluster.add(d)
+            for i, d in enumerate(X):
+                rank = _rank_centroids(d, centroids)
+                violate_constraint = True
+                for idx in rank:
+                    other_clusters = [clusters[j] for j in range(self.n_clusters) if j != idx]
+                    violate_constraint = _violate_constraints(d, clusters[idx], other_clusters, must_link, cannot_link)
+                    # violate_constraint = _violate_constraints(d, clusters[idx], must_link, cannot_link)
+                    if not violate_constraint:
+                        clusters[idx].append(d)
+                        labels[i] = idx
+
+                        inertia += _dist(centroids[idx], d)
                         break
+                if violate_constraint:
+                    raise IOError("Unable to cluster")
 
-            for cluster in clusters:
-                cluster.recalculate()
+            for idx in range(self.n_clusters):
+                centroids[idx] = np.array(clusters[idx]).mean(axis=0)
 
-        self.debug(20*"-")
-        for cluster in clusters:
-            self.debug("cluster: "+str(cluster))
-            self.debug("Instaces in the group: ")
-            self.debug(cluster.instances)
-            self.debug(20*"-")
-        # ...
 
-    @staticmethod
-    def _rank_clusters(instance, clusters):
-        """
-        Return a ascendant list of nearest clusters of certain instance
-        :param instance: Instance to compare
-        :param clusters: Clusters
-        :return: the clusters array sorted by distance of instance
-        """
-        clusters_ranks = np.asarray([np.linalg.norm(p.position - instance) for p in clusters]).argsort()
-        sorted_clusters = np.asarray(clusters)[clusters_ranks]
+        self.cluster_centers_ = centroids
+        self.labels_ = labels
+        self.inertia_ = inertia
 
-        return sorted_clusters
+        return self
 
-    # TODO :soh esta funcionando com numero de variaveis = 1 quand tem links.
-    @staticmethod
-    def _violate_constraints(instance, cluster, must_link, cannot_link):
-        if len(cluster.instances) == 0:
-            return False
+    def predict(self, X):
+        def predict_single(d):
+            return _rank_centroids(d, self.cluster_centers_)[0]
 
-        for link in filter(lambda l: instance in l, must_link):
-            other_instance = np.setdiff1d(link, [instance])
-            if other_instance not in cluster.instances:
-                return True
+        return map(predict_single, X)
 
-        for link in filter(lambda l: instance in l, cannot_link):
-            other_instance = np.setdiff1d(link, [instance])
-            if other_instance in cluster.instances:
-                return True
+    def transform(self, X):
+        def transform_single(d):
+            return np.array([ _dist(d, c) for c in self.cluster_centers_])
 
+        return map(transform_single, X)
+
+
+def _dist(a, b=0):
+    d = a - b
+    return np.sqrt(d.dot(d))
+
+def _rank_centroids(instance, centroids):
+    """
+    Return a ascendant list of nearest clusters of certain instance
+    :param instance: Instance to compare
+    :param centroids: Centroids
+    :return: the clusters array sorted by distance of instance
+    """
+    deltas = centroids - instance
+    rank = np.asarray([_dist(d) for d in deltas]).argsort()
+
+    return rank
+
+def _violate_constraints(instance, cluster, other_clusters, must_link, cannot_link):
+    for link in must_link:
+        other_instance = None
+        if (link[0] == instance).all():
+            other_instance = link[1]
+        elif (link[1] == instance).all():
+            other_instance = link[0]
+        if other_instance is not None:
+            for c in other_clusters:
+                if _contains(other_instance, c):
+                    return True
+
+    for link in cannot_link:
+        other_instance = None
+        if (link[0] == instance).all():
+            other_instance = link[1]
+        elif (link[1] == instance).all():
+            other_instance = link[0]
+        if other_instance is not None and _contains(other_instance, cluster):
+            return True
+
+    return False
+
+
+def _contains(instance, _list):
+    """Returns True if the ndarray list contains the specified instance"""
+    if not _list:
         return False
+    return bool([y for y in (instance == _list) if y.all()])
 
-if __name__ == '__main__':
-    c = ConstrainedKMeans(n_clusters=3, debug=True)
-    X = np.array([[p] for p in range(100)])
-    c.fit(X)
+
+def generate_must_cannot_links(dataset, size=2):
+    must_link = []
+    cannot_link = []
+
+    samples = np.random.choice(len(dataset.data), size)
+
+    for sample in samples:
+        value = dataset.target[sample]
+
+
+
+
+iris = datasets.load_iris()
+
+clf = ConstrainedKMeans(n_clusters=3, debug=False)
+
+links = {
+    'must_link': [
+        [
+            iris.data[0],
+            iris.data[50]
+        ]
+    ],
+    'cannot_link': [
+        [
+            iris.data[0],
+            iris.data[2]
+        ]
+    ]
+}
+
+# np.save('/tmp/123', links)
+# links = np.load('/tmp/123.npy').item()
+
+clf.fit(iris.data, iris.target, **links)
+print clf.labels_
+
+print clf.labels_[0], clf.labels_[50]
